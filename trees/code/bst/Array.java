cf2562bd0293a3f8685cece7af2aec7939cd8383
sujitkc <sujitkc@gmail.com>
2018-01-23 11:31:36 +0530

    improved Array.java

--- trees/code/bst/Array.java
+++ trees/code/bst/Array.java
@@ -7,9 +7,20 @@ class Array {
   public final int numberOfColumns;
   public final int numberOfRows;
 
+  public Array() {
+    this.numberOfRows = 1;
+    this.numberOfColumns = 3;
+    this.array = new Character[this.numberOfRows][this.numberOfColumns];
+    this.array[0][0] = ' ';
+    this.array[0][1] = ' ';
+    this.array[0][2] = ' ';
+    this.title = "";
+  }
+
   public Array(BSTNode node) {
-    Array left = null;
-    Array right = null;
+    Array left = new Array();
+    Array right = new Array();
+
     if(node.getLeft() != null) {
       left = new Array(node.getLeft());
     }
@@ -17,22 +28,10 @@ class Array {
       right = new Array(node.getRight());
     }
     this.title = node.getValue().toString();
-    if(left == null && right == null) {
-      this.numberOfColumns = title.length();
-      this.numberOfRows = 1;
-    }
-    else if (left == null) {
-      this.numberOfColumns = Array.max(title.length(), right.numberOfColumns + 2);
-      this.numberOfRows = right.numberOfRows + 4;
-    }
-    else if (right == null) {
-      this.numberOfColumns = Array.max(title.length(), left.numberOfColumns + 2);
-      this.numberOfRows = left.numberOfRows + 4;
-    }
-    else {
-      this.numberOfColumns = Array.max(title.length(), left.numberOfColumns + right.numberOfColumns + 1);
-      this.numberOfRows = Array.max(left.numberOfRows, right.numberOfRows) + 4;
-    }
+
+    this.numberOfColumns = Array.max(title.length(), left.numberOfColumns + right.numberOfColumns + 1);
+    this.numberOfRows = Array.max(left.numberOfRows, right.numberOfRows) + 4;
+
     this.array = new Character[this.numberOfRows][this.numberOfColumns];
 
     for(int row = 0; row < this.numberOfRows; row++) {
@@ -40,57 +39,38 @@ class Array {
         this.array[row][col] = ' ';
       }
     }
+
+    // place hangers
+    this.array[1][this.numberOfColumns / 2] = '|';
+
+    int startBars;
+    int numberOfBars;
+
+    startBars = left.numberOfColumns / 2;
+    numberOfBars = (left.numberOfColumns / 2) + 3 + (right.numberOfColumns / 2);
+    for(int i = 0; i < numberOfBars; i++) {
+      this.array[2][startBars + i] = '-';
+    }
+    this.array[3][startBars] = '|';
+    this.array[3][startBars + numberOfBars - 1] = '|';
+    for(int row = 0; row < left.numberOfRows; row++) {
+      for(int col = 0; col < left.numberOfColumns; col++) {
+        this.array[row + 4][col] = left.get(row, col);
+      }
+    }
+    // transfer right
+    int xshift = 1;
+    xshift += left.numberOfColumns;
+    for(int row = 0; row < right.numberOfRows; row++) {
+      for(int col = 0; col < right.numberOfColumns; col++) {
+        this.array[row + 4][col + xshift] = right.get(row, col);
+      }
+    }
+    
     // place title
     int startTitle = (this.numberOfColumns - title.length()) / 2;
     for(int i = 0; i < title.length(); i++) {
       this.array[0][startTitle + i] = title.charAt(i);
-    }
-    // place hangers
-    if(left != null || right != null) {
-      this.array[1][this.numberOfColumns / 2] = '|';
-
-      int startBars;
-      int numberOfBars;
-      if(left == null && right != null) {
-        startBars = (this.numberOfColumns / 2) - 1;
-        numberOfBars = 2 + (right.numberOfColumns / 2);
-      }
-      else if(left != null && right == null) {
-        startBars = left.numberOfColumns / 2;
-        numberOfBars = 2 + (left.numberOfColumns / 2);
-      }
-      else {
-        startBars = left.numberOfColumns / 2;
-        numberOfBars = (left.numberOfColumns / 2) + 2 + (right.numberOfColumns / 2);
-      }
-      for(int i = 0; i < numberOfBars; i++) {
-        this.array[2][startBars + i] = '-';
-      }
-      this.array[3][startBars] = '|';
-      this.array[3][startBars + numberOfBars - 1] = '|';
-    }
-    // transfer left
-    if(left != null) {
-      for(int row = 0; row < left.numberOfRows; row++) {
-        for(int col = 0; col < left.numberOfColumns; col++) {
-          this.array[row + 4][col] = left.get(row, col);
-        }
-      }
-    }
-    // transfer right
-     if(right != null) {
-      int xshift = 1;
-      if(left != null) {
-        xshift += left.numberOfColumns;
-      }
-      else {
-        xshift += 1;
-      }
-      for(int row = 0; row < right.numberOfRows; row++) {
-        for(int col = 0; col < right.numberOfColumns; col++) {
-          this.array[row + 4][col + xshift] = right.get(row, col);
-        }
-      }
     }
   }
 
